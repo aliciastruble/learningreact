92270b176f1917f9982f1bcbe383a6fa1a823cc5
name <aya.ablaze@gmail.com>
2019-10-05 18:32:01 -0500

    Add routing

---
+++ src/components/common/navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/movies">
+        Vidly
+      </Link>
+      <div className="collapse navbar-collapse">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/movies">
+            Movies
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/customers">
+            Customers
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/rentals">
+            Rentals
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
